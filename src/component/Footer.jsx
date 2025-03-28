4f10a3cf35150289a832e1509bd1443ff942b7dd
Deny <sokundeny18@gmail.com>
2025-03-29 01:18:28 +0700

    nothing

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-screen bg-secondary h-10  px-10 sm:px-20 lg:px-32 py-10 flex  items-center justify-between overflow-hidden'>
+        
+    </div>
   )
 }
 
