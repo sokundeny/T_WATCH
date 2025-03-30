a146bc7b49bc3342d23b1d05b2c66380dbd6998a
sothearasaroeun <sotheara.saroeun.911@gmail.com>
2025-03-30 22:22:19 +0700

    Updated Ver.

--- src/constant/main.js
+++ src/constant/main.js
@@ -44,17 +44,55 @@ import Card41 from "../assets/image/zephyr_gold.png";
 //Accessories DATA//
 import box1 from "../assets/image/Box1.png";
 import box2 from "../assets/image/Box2.png";
+import box3 from "../assets/image/box3.png";
+import box4 from "../assets/image/box4.png";
+import box5 from "../assets/image/box5.png";
+import box6 from "../assets/image/box6.png";
+import box7 from "../assets/image/box7.png";
+import box8 from "../assets/image/box8.png";
+import box9 from "../assets/image/box9.png";
+import box10 from "../assets/image/box10.png";
+import box11 from "../assets/image/box11.png";
+import box12 from "../assets/image/box12.png";
+import box13 from "../assets/image/box13.png";
+import box14 from "../assets/image/box14.png";
+import box15 from "../assets/image/box15.png";
+import box16 from "../assets/image/box16.png";
+import box17 from "../assets/image/box17.png";
+import box18 from "../assets/image/box18.png";
+import box19 from "../assets/image/box19.png";
+import box20 from "../assets/image/box20.png";
+import box21 from "../assets/image/box21.png";
+import box22 from "../assets/image/box22.png";
+import box23 from "../assets/image/box23.png";
+import box24 from "../assets/image/box24.png";
+import box25 from "../assets/image/box25.png";
 
 //Straps Data//
-
 import strap1 from "../assets/image/strap1.png";
 import strap2 from "../assets/image/strap2.png";
 import strap3 from "../assets/image/strap3.png";
-
-
-
-
-
+import strap4 from "../assets/image/strap4.png";
+import strap5 from "../assets/image/strap5.png";
+import strap6 from "../assets/image/strap6.png";
+import strap7 from "../assets/image/strap7.png";
+import strap8 from "../assets/image/strap8.png";
+import strap9 from "../assets/image/strap9.png";
+import strap10 from "../assets/image/strap10.png";
+import strap11 from "../assets/image/strap11.png";
+import strap12 from "../assets/image/strap12.png";
+import strap13 from "../assets/image/strap13.png";
+import strap14 from "../assets/image/strap14.png";
+import strap15 from "../assets/image/strap15.png";
+import strap16 from "../assets/image/strap16.png";
+import strap17 from "../assets/image/strap17.png";
+import strap18 from "../assets/image/strap18.png";
+import strap19 from "../assets/image/strap19.png";
+import strap20 from "../assets/image/strap20.png";
+import strap21 from "../assets/image/strap21.png";
+import strap22 from "../assets/image/strap22.png";
+import strap23 from "../assets/image/strap23.png";
+import strap24 from "../assets/image/strap24.png";
 
 export const imageData = [
   { imageSrc: Card1, name: "Card1", category: "Women", tag: "NEW RELEASE" },
@@ -111,16 +149,56 @@ export const navmenu=[
 ];
 
 export const Boxdata = [
-  { image:box1 , name: "Card ",price: "490$" },
-  { image:box2 , name: "Card2",price: "490$"},
-  { image:box1 , name: "Card3",price: "490$" },
-  { image:box2 , name: "Card4",price: "490$" },
-  { image:box1 , name: "Card5",price: "490$" },
-  { image:box2, name: "Card6",price: "490$" },
+  { image:box1 , name: "WB - WATCH WINDER LED 4-5 BROWN", price: "$390" },
+  { image:box2 , name: "WB - WATCH WINDER LED 4-5 CARBON", price: "$390"},
+  { image:box3 , name: "BLACK LEATHER DOUBLE WATCH ROTATOR", price: "$399" },
+  { image:box4 , name: "BROWN LEATHER DOUBLE WATCH ROTATOR", price: "$399" },
+  { image:box5 , name: "LEATHER WATCH CASE 4 SLOTS", price: "$129" },
+  { image:box6 , name: "BLACK CROCODILE LEATHER WATCH CASE 6 SLOTS", price: "&111" },
+  { image:box7 , name: "BLACK LEATHER WATCH CASE 20 SLOTS", price: "$149" },
+  { image:box8 , name: "HARD CRAFT WATCH BOX 20 SLOTS", price: "$299"},
+  { image:box9 , name: "LUXURY WATCH BOX BURLWOOD FINISH WOOD GLASS WINDOW LOCK", price: "$169" },
+  { image:box10 , name: "BROMPTON WATCH COLLECTOR BOX", price: "$179" },
+  { image:box11 , name: "DANIEL WELLINGTON DISPLAY WOOD CASE", price: "$169" },
+  { image:box12 , name: "CARBON FIBER JEWELRY DISPLAY CASE", price: "$169" },
+  { image:box13 , name: "ALUMINIUM WATCH CASE 24 SLOTS", price: "$143" },
+  { image:box14 , name: "BLACK LEATHER WATCH BOX 10 SLOTS", price: "$139"},
+  { image:box15 , name: "JACOB JONES WATCH BOX 8 SLOTS", price: "$189" },
+  { image:box16 , name: "LEATHERETTE WATCH BOX 3 SLOTS", price: "$145" },
+  { image:box17 , name: "ALUMINIUM WATCH BOX 6 SLOTS", price: "$185" },
+  { image:box18 , name: "RED LEATHER WATCH BOX FOR WOMEN 6 SLOTS", price: "$179" },
+  { image:box19 , name: "RED WOOD WATCH BOX 5 SLOTS", price: "189.50$" },
+  { image:box20 , name: "VINTAGE WATCH BOX IN DARK WOOD 3 SLOTS", price: "$169"},
+  { image:box21 , name: "AUTOMATIC WATCH WINDING BOX", price: "$99" },
+  { image:box22 , name: "UNISEX DUO COMPARTMENT WATCH BOX", price: "$89" },
+  { image:box23 , name: "UTEN AUTOMATIC ROTATION WATCH WINDER", price: "$85" },
+  { image:box24 , name: "RED WOOD WATCH BOX 2 SLOTS", price: "$179" },
+  { image:box25 , name: "HANDMADE WOODEN BOX", price: "$69" },
 ];
+
 export const Strapsdata = [
-  { image:strap1 , name: "Card ",price: "490$" },
-  { image:strap2 , name: "Card2",price: "490$"},
-  { image:strap3 , name: "Card3",price: "490$" },
-  { image:strap1 , name: "Card4",price: "490$" },
+  { image:strap1 , name: "STR BLACK LEATHER STRAP", price: "$40" },
+  { image:strap2 , name: "STR BLAZE ORANGE RUBBER STRAP", price: "$40"},
+  { image:strap3 , name: "STR CARBON LEATHER STRAP", price: "$50" },
+  { image:strap4 , name: "STR CAMO GREY STRAP", price: "$50" },
+  { image:strap5 , name: "STR CROCODILE BLUE STAMP STRAP", price: "$59" },
+  { image:strap6 , name: "STR CROCODILE RED STAMP STRAP", price: "$59"},
+  { image:strap7 , name: "STR CROCODILE BLACK STAMP STRAP", price: "$59" },
+  { image:strap8 , name: "STR Z6 CROSA LEATHER STRAP", price: "$59" },
+  { image:strap9 , name: "STR LV WHITE STRAP", price: "$49" },
+  { image:strap10 , name: "STR GUCCI GREY BLACK G STRAP", price: "$49"},
+  { image:strap11 , name: "STR M1 RED STRAP", price: "$49" },
+  { image:strap12 , name: "STR M1 BLUE STRAP", price: "$49" },
+  { image:strap13 , name: "STR Q1 BLACK CARBON STRAP", price: "$59" },
+  { image:strap14 , name: "STR Q2 GREEN CARBON STRAP", price: "$59"},
+  { image:strap15 , name: "STR Q3 BLUE CARBON STRAP", price: "$59" },
+  { image:strap16 , name: "STR Q4 RED CARBON STRAP", price: "$59" },
+  { image:strap17 , name: "STR S12 WHITE BLUE RUBBER STRAP", price: "$49" },
+  { image:strap18 , name: "STR S13 GREEN RUBBER STRAP", price: "$49"},
+  { image:strap19 , name: "STR S6 CORSA RUBBER STRAP", price: "$49" },
+  { image:strap20 , name: "STR S7 MIAMI BLUE RUBBER STRAP", price: "$49" },
+  { image:strap21 , name: "STR WOMEN DARK RED RUBBER STRAP", price: "$49" },
+  { image:strap22 , name: "STR WOMEN GREY RUBBER STRAP", price: "$49"},
+  { image:strap23 , name: "STR WOMEN RED RUBBER STRAP", price: "$49" },
+  { image:strap24 , name: "STR WOMEN BLACK LEATHER STRAP", price: "$49" },
 ];
